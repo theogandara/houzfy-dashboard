a664a220e9e5eff64e2c0f5a72de437a1a8cad95
Théo Gândara <theogandara@gmail.com>
2024-06-30 02:05:54 +0100

    Feat: added .env and .env.example

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:3000/",
+  baseURL: process.env.REACT_APP_API_URL,
   timeout: 5000,
   headers: {
     authorization: `Bearer ${localStorage.getItem("jwt")}`,
