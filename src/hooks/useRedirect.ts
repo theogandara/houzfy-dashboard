f1c82dc7ce5b3ca03ee28d3336bb716c8886c15f
Théo Gândara <theogandara@gmail.com>
2024-07-07 21:51:49 +0100

    Feat: useRedirect goBack

--- src/hooks/useRedirect.ts
+++ src/hooks/useRedirect.ts
@@ -3,9 +3,13 @@ import { useNavigate } from "react-router-dom";
 export const useRedirect = () => {
   const navigate = useNavigate();
 
+  const goBack = () => {
+    navigate(-1);
+  };
+
   const navigateTo = (path: string) => {
     navigate(path);
   };
 
-  return { navigateTo };
+  return { navigateTo, goBack };
 };
