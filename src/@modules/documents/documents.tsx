f350466a461a6f2b2ad32070742ae950958fb8a6
Théo Gândara <theogandara@gmail.com>
2024-07-08 11:39:57 +0100

    Feat: mock documents and DocCard component

--- src/@modules/documents/documents.tsx
+++ src/@modules/documents/documents.tsx
@@ -1,11 +1,82 @@
 import LayoutDefault from "../../layouts/LayoutDefault";
 import { Subtitle, Title } from "../../components/Texts/Texts";
+import { Flex, Grid } from "@chakra-ui/react";
+import { FilePdf } from "@phosphor-icons/react";
+
+const documentsMock = [
+  {
+    id: "1",
+    title: "Contrato Aluguel",
+    description: "Locação de imóvel residencial",
+  },
+  {
+    id: "2",
+    title: "Contrato Venda",
+    description: "Compra e venda de imóvel residencial",
+  },
+];
 
 export const Documents = () => {
   return (
     <LayoutDefault>
-      <Title>Documentos</Title>
-      <Subtitle>Your current sales summary and activity.</Subtitle>
+      <Flex flexDir="column" gap="16px" overflow="auto">
+        <Flex flexDir="column" gap="4px">
+          <Title>Documentos</Title>
+          <Subtitle>Your current sales summary and activity.</Subtitle>
+        </Flex>
+
+        <Grid
+          templateColumns={{
+            mobile: "repeat(1, 1fr)",
+            tablet: "repeat(2, 1fr)",
+            desktop: "repeat(3, 1fr)",
+          }}
+          gap="16px"
+        >
+          {documentsMock.map((document) => (
+            <DocumentCard
+              id={document.id}
+              title={document.title}
+              description={document.description}
+              key={document.id}
+            />
+          ))}
+        </Grid>
+      </Flex>
     </LayoutDefault>
+  );
+};
+
+const DocumentCard = ({
+  id,
+  title,
+  description,
+}: {
+  id: string;
+  title: string;
+  description: string;
+}) => {
+  return (
+    <Flex
+      borderRadius="4px"
+      border="2px solid #EAECF0"
+      p="16px"
+      flexDir="column"
+      gap="8px"
+      cursor="pointer"
+    >
+      <Flex
+        flexDir="column"
+        gap="4px"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <FilePdf size={96} weight="duotone" />
+        <Subtitle textAlign="center">{title}</Subtitle>
+        <Subtitle textAlign="center" color="text.secondary">
+          {description}
+        </Subtitle>
+      </Flex>
+    </Flex>
   );
 };
