645e4435ebfb27a8a8c89c7d46e99e23f4115d35
Théo Gândara <theogandara@gmail.com>
2024-07-09 23:33:26 +0100

    Feat: works on new property details

--- src/@modules/propertyDetails/Details.tsx
+++ src/@modules/propertyDetails/Details.tsx
@@ -1,4 +1,4 @@
-import { Button, Flex, useToast } from "@chakra-ui/react";
+import { Button, Flex, useDisclosure, useToast } from "@chakra-ui/react";
 import { P, Subtitle, Title } from "../../components/Texts/Texts";
 import { useRedirect } from "../../hooks/useRedirect";
 import LayoutHeader from "../../layouts/LayoutHeader";
@@ -8,11 +8,14 @@ import { propertiesService } from "../properties/service/service";
 import { useMutation, useQuery } from "react-query";
 import { ErrorPage } from "../../components/Feedback/ErrorPage";
 import { LoadingContent } from "../../components/Feedback/LoadingContent";
+import { ModalEditProperty } from "./ModalEditProperty";
 
 export const HouseDetails = () => {
   const { navigateTo, goBack } = useRedirect();
   const { id } = useParams();
   const toast = useToast();
+
+  const modalEditProperty = useDisclosure();
 
   const deleteProperty = useMutation({
     mutationFn: () => propertiesService.deleteProperty(id || ""),
@@ -65,84 +68,90 @@ export const HouseDetails = () => {
     );
 
   return (
-    <LayoutHeader title="Detalhes do imóvel" onClickBack={goBack}>
-      <Flex flexDir="column" gap="16px" overflow="auto">
-        <Flex flexDir="column" gap="4px">
-          <Title>Detalhes do imóvel</Title>
-          <Subtitle>Veja os detalhes do imóvel que você selecionou.</Subtitle>
-        </Flex>
-
-        <Flex
-          borderRadius="4px"
-          flexDir={{ mobile: "column", tablet: "row" }}
-          gap="16px"
-        >
-          <Flex p="16px" flexDir="column" gap="8px">
-            <Subtitle>Informações básicas</Subtitle>
-
-            <Flex flexDir="column" gap="8px">
-              <Item label="Título" value={property.title} />
-              <Item label="Descrição" value={property.description} />
-              <Item label="Preço" value={property.price} />
-              <Item label="Finalidade" value={property.purpose} />
-              <Item label="Categoria" value={property.category} />
-            </Flex>
+    <>
+      <ModalEditProperty
+        isOpen={modalEditProperty.isOpen}
+        onClose={modalEditProperty.onClose}
+      />
+      <LayoutHeader title="Detalhes do imóvel" onClickBack={goBack}>
+        <Flex flexDir="column" gap="16px" overflow="auto">
+          <Flex flexDir="column" gap="4px">
+            <Title>Detalhes do imóvel</Title>
+            <Subtitle>Veja os detalhes do imóvel que você selecionou.</Subtitle>
           </Flex>
 
-          <Flex p="16px" flexDir="column" gap="8px">
-            <Subtitle>Localização</Subtitle>
-
-            <Flex flexDir="column" gap="8px">
-              <Item label="Endereço" value={property.address} />
-              <Item label="Número" value={property.number} />
-              <Item label="Bairro" value={property.neighborhood} />
-              <Item label="Cidade" value={property.city} />
-              <Item label="Estado" value={property.state} />
-              <Item label="CEP" value={property.zipCode} />
-              <Item label="Complemento" value="" />
-            </Flex>
-          </Flex>
-
-          <Flex p="16px" flexDir="column" gap="8px">
-            <Subtitle>Detalhes do imóvel</Subtitle>
-
-            <Flex flexDir="column" gap="8px">
-              <Item label="Área" value={property.area} />
-              <Item label="Quartos" value={property.bedrooms} />
-              <Item label="Banheiros" value={property.bathrooms} />
-              <Item label="Vagas de garagem" value={property.parkingSpaces} />
-              <Item
-                label="Elevador"
-                value={property.elevator ? "Sim" : "Não"}
-              />
-              <Item
-                label="Mobiliado"
-                value={property.furnished ? "Sim" : "Não"}
-              />
-              <Item
-                label="Portaria 24h"
-                value={property.doorman ? "Sim" : "Não"}
-              />
-            </Flex>
-          </Flex>
-        </Flex>
-
-        <Flex justify="space-between">
-          <Button colorScheme="blue">Salvar</Button>
-
-          <Button
-            colorScheme="red"
-            display="flex"
-            alignItems="center"
-            gap="8px"
-            onClick={() => deleteProperty.mutate()}
+          <Flex
+            borderRadius="4px"
+            flexDir={{ mobile: "column", tablet: "row" }}
+            gap="16px"
           >
-            <Trash />
-            Excluir imóvel
-          </Button>
+            <Flex p="16px" flexDir="column" gap="8px">
+              <Subtitle>Informações básicas</Subtitle>
+
+              <Flex flexDir="column" gap="8px">
+                <Item label="Título" value={property.title} />
+                <Item label="Descrição" value={property.description} />
+                <Item label="Preço" value={property.price} />
+                <Item label="Finalidade" value={property.purpose} />
+                <Item label="Categoria" value={property.category} />
+              </Flex>
+            </Flex>
+
+            <Flex p="16px" flexDir="column" gap="8px">
+              <Subtitle>Localização</Subtitle>
+
+              <Flex flexDir="column" gap="8px">
+                <Item label="Endereço" value={property.address} />
+                <Item label="Número" value={property.number} />
+                <Item label="Bairro" value={property.neighborhood} />
+                <Item label="Cidade" value={property.city} />
+                <Item label="Estado" value={property.state} />
+                <Item label="CEP" value={property.zipCode} />
+                <Item label="Complemento" value="" />
+              </Flex>
+            </Flex>
+
+            <Flex p="16px" flexDir="column" gap="8px">
+              <Subtitle>Detalhes do imóvel</Subtitle>
+
+              <Flex flexDir="column" gap="8px">
+                <Item label="Área" value={property.area} />
+                <Item label="Quartos" value={property.bedrooms} />
+                <Item label="Banheiros" value={property.bathrooms} />
+                <Item label="Vagas de garagem" value={property.parkingSpaces} />
+                <Item
+                  label="Elevador"
+                  value={property.elevator ? "Sim" : "Não"}
+                />
+                <Item
+                  label="Mobiliado"
+                  value={property.furnished ? "Sim" : "Não"}
+                />
+                <Item
+                  label="Portaria 24h"
+                  value={property.doorman ? "Sim" : "Não"}
+                />
+              </Flex>
+            </Flex>
+          </Flex>
+
+          <Flex justify="space-between">
+            <Button colorScheme="blue">Salvar</Button>
+
+            <Button
+              colorScheme="red"
+              display="flex"
+              alignItems="center"
+              gap="8px"
+              onClick={() => deleteProperty.mutate()}
+            >
+              <Trash />
+              Excluir imóvel
+            </Button>
+          </Flex>
         </Flex>
-      </Flex>
-    </LayoutHeader>
+      </LayoutHeader>
+    </>
   );
 };
 
---
+++ src/@modules/propertyDetails/ModalEditProperty.tsx
@@ -0,0 +1,29 @@
+import {
+  Button,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+} from "@chakra-ui/react";
+import { BasicInfos } from "../newProperty/form/BasicInfos";
+
+export const ModalEditProperty = ({ isOpen, onClose }: any) => {
+  return (
+    <Modal onClose={onClose} size="xl" isOpen={isOpen}>
+      <ModalOverlay />
+      <ModalContent>
+        <ModalHeader>Modal Title</ModalHeader>
+        <ModalCloseButton />
+        <ModalBody>
+          <BasicInfos />
+        </ModalBody>
+        <ModalFooter>
+          <Button onClick={onClose}>Close</Button>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
+};
